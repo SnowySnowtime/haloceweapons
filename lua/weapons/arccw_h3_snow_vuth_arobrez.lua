2885cde1304a51bf4fe149f098e0c2eedbafe7e4
Fesiug <publicfesiug@outlook.com>
2020-10-19 13:20:35 -0400

    I'm insane

---
+++ lua/weapons/arccw_h3_snow_vuth_arobrez.lua
@@ -0,0 +1,328 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - Halo 3" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "Assault Rifle Obrez"
+SWEP.TrueName = "MA5C_XO- ICWS"
+SWEP.Trivia_Class = "Assault Rifle"
+SWEP.Trivia_Desc = "Experimental shortened version of the MA5C."
+SWEP.Trivia_Manufacturer = "Misriah Armory"
+SWEP.Trivia_Calibre = "7.0x43mm XPE HP"
+SWEP.Trivia_Mechanism = "Automatic"
+SWEP.Trivia_Country = "Obama"
+
+SWEP.Slot = 3
+
+if GetConVar("arccw_truenames"):GetBool() then SWEP.PrintName = SWEP.TrueName end
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/snowysnowtime/vuthakral/fesiug/c_hum_ma5co_4.mdl"
+SWEP.WorldModel = "models/snowysnowtime/vuthakral/w_ma5c.mdl"
+SWEP.ViewModelFOV = 70
+
+if GetConVar("arccw_hce_bal"):GetInt() == 0 then -- HaloCW
+	SWEP.Recoil = 0.2
+	SWEP.RecoilSide = 0.2
+	SWEP.Damage = 15
+	SWEP.DamageMin = 10 -- damage done at maximum range
+	SWEP.AccuracyMOA = 65 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+	SWEP.HipDispersion = 120 -- inaccuracy added by hip firing.
+	SWEP.MoveDispersion = 100
+	SWEP.JumpDispersion = 0
+	SWEP.ChamberSize = 0
+elseif GetConVar("arccw_hce_bal"):GetInt() == 1 then -- halo purist
+	SWEP.Recoil = 0
+	SWEP.RecoilSide = 0
+	SWEP.Damage = 25
+	SWEP.DamageMin = 15 -- damage done at maximum range
+	SWEP.JumpDispersion = 0
+	SWEP.HipDispersion = 0
+	SWEP.MoveDispersion = 0
+	SWEP.ChamberSize = 1
+elseif GetConVar("arccw_hce_bal"):GetInt() == 2 then -- arccw
+    SWEP.Recoil = 0.2
+	SWEP.RecoilSide = 0.2
+	SWEP.Damage = 20
+	SWEP.DamageMin = 6 -- damage done at maximum range
+	SWEP.AccuracyMOA = 70 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+	SWEP.HipDispersion = 120 -- inaccuracy added by hip firing.
+	SWEP.MoveDispersion = 100
+	SWEP.ChamberSize = 1
+end
+
+SWEP.MeleeSwingSound = ""
+SWEP.MeleeMissSound = ""
+SWEP.MeleeHitSound = "hceworld"
+SWEP.MeleeHitNPCSound = "hceslap"
+
+SWEP.Range = 50 -- in METRES
+SWEP.Penetration = 20
+SWEP.DamageType = DMG_BULLET
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 600 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+
+SWEP.TracerNum = 1 -- tracer every X
+
+SWEP.Primary.ClipSize = 20 -- DefaultClip is automatically set.
+SWEP.ExtendedClipSize = 30
+SWEP.ReducedClipSize = 15
+
+SWEP.Delay = 60 / 650 * 0.8 -- 60 / RPM.
+SWEP.Num = 1 -- number of shots per trigger pull.
+SWEP.Firemodes = {
+    {
+        Mode = -4,
+		PostBurstDelay = 60/650,
+		RunawayBurst = true,
+		AutoBurst = true,
+    },
+    {
+        Mode = 0
+    }
+}
+
+SWEP.NPCWeaponType = {"weapon_ar2","weapon_smg"}
+SWEP.NPCWeight = 25
+
+SWEP.ManualAction = false
+
+SWEP.Primary.Ammo = "smg1" -- what ammo type the gun uses
+SWEP.MagID = "hs338" -- the magazine pool this gun draws from
+
+SWEP.ShootVol = 140 -- volume of shoot sound
+SWEP.ShootPitch = 100 -- pitch of shoot sound
+
+SWEP.ShootSound = Sound("ar_h3_fire")
+SWEP.ShootSoundSilenced = "weapons/arccw/m4a1/m4a1_silencer_01.wav"
+SWEP.DistantShootSound = "ar_lod"
+
+SWEP.MuzzleEffect = "astw2_halo_3_muzzle_assault_rifle"
+SWEP.ShellModel = "models/shells/shell_338mag.mdl"
+SWEP.ShellPitch = 80
+SWEP.ShellScale = 1.5
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+
+SWEP.SightTime = 0.3
+SWEP.SpeedMult = 0.9
+SWEP.SightedSpeedMult = 0.2
+SWEP.MeleeTime = 0.8
+
+SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
+    -- [0] = "bulletchamber",
+    -- [1] = "bullet1"
+}
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+
+SWEP.CaseBones = {}
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-3.375, -3, 0),
+    Ang = Angle(0.792, 0.017, 0),
+    Magnification = 1.1,
+}
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "ar2"
+SWEP.HoldtypeSights = "rpg"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+
+SWEP.ActivePos = Vector(0, -2, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.HolsterPos = Vector(3, -3, -0.5)
+SWEP.HolsterAng = Angle(-10, 30, -10)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+
+SWEP.CustomizePos = Vector(2.824, -1, -2.897)
+SWEP.CustomizeAng = Angle(12.149, 30.547, 0)
+
+SWEP.BarrelLength = 30
+SWEP.AttachmentElements = {
+	["gold"] = {
+        VMSkin = 1,
+        WMSkin = 1,
+    },
+    ["squirt"] = {
+        VMSkin = 2,
+        WMSkin = 2,
+    },
+	["corrosion"] = {
+        VMSkin = 3,
+        WMSkin = 3,
+    },
+	["woodland"] = {
+        VMSkin = 4,
+        WMSkin = 4,
+    },
+}
+
+SWEP.ExtraSightDist = 5
+
+SWEP.Attachments = {
+	{
+        PrintName = "Optic", -- print name
+        DefaultAttName = "10x Scope",
+        Slot = {"optic", "optic_lp"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "gun", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(10, 0, 7.4), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 0),
+            wpos = Vector(6, 2, -4.4),
+            wang = Angle(-8.829, 0, 180)
+        },
+        CorrectivePos = Vector(0, 0, 0),
+        CorrectiveAng = Angle(0, 0, 0),
+        InstalledEles = {"mount"}
+    },
+	{
+        PrintName = "Underbarrel",
+        Slot = {"foregrip", "bipod", "style_pistol"},
+        Bone = "gun",
+        Offset = {
+            vpos = Vector(14, 0, 2),
+            vang = Angle(0, 0, 0),
+            wpos = Vector(13, 0.6, -3.5),
+            wang = Angle(-10, 0, 180),
+        },		
+    },
+    {
+        PrintName = "Grip",
+        Slot = "grip",
+        DefaultAttName = "Standard Grip"
+    },	
+    {
+        PrintName = "Stock",
+        Slot = "stock",
+        DefaultAttName = "Standard Stock"
+    },
+	{
+        PrintName = "Firetype",
+        Slot = {"fcg","fcg_smg"}
+    },
+	{
+        PrintName = "Tactical",
+        Slot = "tac",
+        Bone = "gun",
+        Offset = {
+            vpos = Vector(17, -0.8, 5), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 90),
+            wpos = Vector(6, 1.25, -3),
+            wang = Angle(-8.829, -0.556, 90)
+        },
+    },
+    {
+        PrintName = "Ammo Type",
+        Slot = "ammo_bullet"
+    },
+    {
+        PrintName = "Perk",
+        Slot = {"perk","go_perk"}
+    },
+    {
+        PrintName = "Charm",
+        Slot = "charm",
+        FreeSlot = true,
+        Bone = "gun", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(3.5, -0.6, 5.25), -- offset that the attachment will be relative to the bone
+            vang = Angle(0, 0, 10),
+            wpos = Vector(6, 2.4, -3.5),
+            wang = Angle(-10.393, 0, 180)
+        },
+    },
+	{
+        PrintName = "Skin",
+        Slot = {"skin_h3ar"},
+        DefaultAttName = "Factory Default",
+        FreeSlot = true
+    },
+}
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 99/30
+    },
+	["fire_iron"] = {
+        Source = "fire",
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 19/30,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 0.25,
+    },
+    ["fire"] = {
+        Source = { "fire_rand1","fire_rand2","fire_rand3" },
+		Time = 4/30,
+    },
+	["bash"] = {
+        Source = { "melee_rand0", "melee_rand1" },
+		Time = 29/30 * 0.8,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 0.2,
+    },
+    ["reload"] = {
+        Source = "reload",
+        Time = 57/30,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        Checkpoints = {24, 33, 51},
+        FrameRate = 30,
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 0.5,
+    },
+    ["reload_empty"] = {
+        Source = "reload",
+        Time = 57/30,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        Checkpoints = {24, 33, 51},
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 0.5,
+    },
+}
+
+-- nZombies Stuff
+SWEP.NZWonderWeapon		= false	-- Is this a Wonder-Weapon? If true, only one player can have it at a time. Cheats aren't stopped, though.
+--SWEP.NZRePaPText		= "your text here"	-- When RePaPing, what should be shown? Example: Press E to your text here for 2000 points.
+SWEP.NZPaPName				= "Fight Finisher"
+--SWEP.NZPaPReplacement 	= ""	-- If Pack-a-Punched, replace this gun with the entity class shown here.
+SWEP.NZPreventBox		= false	-- If true, this gun won't be placed in random boxes GENERATED. Users can still place it in manually.
+SWEP.NZTotalBlackList	= false	-- if true, this gun can't be placed in the box, even manually, and can't be bought off a wall, even if placed manually. Only code can give this gun.
+
+SWEP.Primary.MaxAmmo = 352
+-- Max Ammo function
+
+function SWEP:NZMaxAmmo()
+
+	local ammo_type = self:GetPrimaryAmmoType() or self.Primary.Ammo
+
+    if SERVER then
+        self.Owner:SetAmmo( self.Primary.MaxAmmo, ammo_type )
+    end
+end
+
+-- PaP Function
+function SWEP:OnPaP()
+self.Ispackapunched = 1
+self.PrintName = "Fight Finisher"
+self.Primary.ClipSize = 32
+self.Primary.MaxAmmo = 352
+self.Damage = 35
+self.DamageMin = 25
+self.Delay = 60 / 700
+return true
+end
