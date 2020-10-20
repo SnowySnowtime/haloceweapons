fb644cddd8524acd47785e9097425146768c2468
Snowy Snowtime <snowysnowtime1@gmail.com>
2020-10-20 08:45:10 -0700

    now using drc matproxies over ripped ones from it

--- lua/matproxy/halocolor.lua
+++ lua/matproxy/halocolor.lua
@@ -1,22 +1,4 @@
 matproxy.Add( {
-	name = "vuth_Compass",
-	init = function( self, mat, values )
-		self.ResultTo = values.resultvar
-	end,
-
-	bind = function( self, mat, ent )
-		if ( !IsValid( ent )) then return end
-		local owner = ent:GetOwner()
-		if ( !IsValid( owner ) or !owner:IsPlayer() ) then return end
-		local world = game.GetWorld
-		local ang = owner:EyeAngles()
-		local angmath = ang
-
-		mat:SetVector( self.ResultTo, Vector(-angmath.y, 0, 0) )
-	end
-} )
-
-matproxy.Add( {
 	name = "haloPlayerWeaponColorNOPULSE",
 
 	init = function( self, mat, values )
@@ -39,49 +21,5 @@ matproxy.Add( {
 
 		mat:SetVector( self.ResultTo, col + col * mul )
 
-	end
-} )
-
-matproxy.Add( {
-	name = "vuth_Compass",
-	init = function( self, mat, values )
-		self.ResultTo = values.resultvar
-	end,
-
-	bind = function( self, mat, ent )
-		if ( !IsValid( ent )) then return end
-		local owner = ent:GetOwner()
-		if ( !IsValid( owner ) or !owner:IsPlayer() ) then return end
-		local world = game.GetWorld
-		local ang = owner:EyeAngles()
-		local angmath = ang
-
-		mat:SetVector( self.ResultTo, Vector(-angmath.y, 0, 0) )
-	end
-} )
-
-matproxy.Add( {
-	name = "vuth_ReflectionTint",
-	init = function( self, mat, values )
-		self.ResultTo = values.resultvar
-		self.TintVector = mat:GetVector("$cubemaptint")
-		self.PowerFloat = mat:GetFloat("$cubemappower")
-	end,
-
-	bind = function( self, mat, ent )
-		if ( !IsValid( ent )) then return end
-		local owner = ent
-		if !IsValid( owner ) then return end
-		local lightlevel = render.GetLightColor(owner:GetPos())
-		
-		local lightr = lightlevel.x
-		local lightg = lightlevel.y
-		local lightb = lightlevel.z
-		
-		local col = Vector(lightr, lightg, lightb)
-		
-		if ( !isvector( col )) then return end
-		
-		mat:SetVector( self.ResultTo, (col * self.PowerFloat) + (self.TintVector * self.PowerFloat) * col )
 	end
 } )
